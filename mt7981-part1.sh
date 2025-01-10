cb4901a5972c26ed46f25a61690420761b21a25a
dossec <dossec@gmail.com>
2025-01-10 11:06:46 +0800

    Update mt7981-part1.sh

--- mt7981-part1.sh
+++ mt7981-part1.sh
@@ -11,3 +11,6 @@
 # Add a feed source
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
+# vnt
+echo 'src-git vnt https://github.com/lmq8267/luci-app-vnt.git' >>feeds.conf.default
