aaf7df0cbb68db54155d282334dae6aa07dbbc37
dossec <dossec@gmail.com>
2025-01-13 14:49:30 +0800

    Create mt7981_23.05-part1.sh

---
+++ mt7981_23.05-part1.sh
@@ -0,0 +1,19 @@
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
+# vnt
+echo 'src-git vnt https://github.com/lmq8267/luci-app-vnt.git' >>feeds.conf.default
+
+# filetransfer
+echo 'src-git vnt https://github.com/DustReliant/luci-app-filetransfer' >>feeds.conf.default
